aa29850cbc3cb09771b295bd0f1f0b2155d707e7
corcor322 <corcor322@gmail.com>
2024-11-12 11:05:11 -0500

    Built homescreen while loop in UserInterface, completing Exit and Invalid Choice options.

--- src/main/java/com/tasty/Main.java
+++ src/main/java/com/tasty/Main.java
@@ -1,4 +1,9 @@
 package com.tasty;
 
 public class Main {
+    public static void main(String[] args) {
+        UserInterface ui = new UserInterface();
+        ui.display();
+    }
+
 }
--- src/main/java/com/tasty/UserInterface.java
+++ src/main/java/com/tasty/UserInterface.java
@@ -1,9 +1,27 @@
 package com.tasty;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class UserInterface {
     public void display() {
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        while (!quit) {
+        System.out.println("Welcome!");
+        System.out.println("");
+        System.out.println("1) New Order");
+        System.out.println("0) Exit");
+
+        int homeChoice = scanner.nextInt();
+        if (homeChoice == 1) {
+
+        } else if (homeChoice == 0) {
+            quit = true;
+            System.out.println("Enjoy your day! :-)");
+        } else
+            System.out.println("Invalid choice. Please enter a 1 or a 0.");
+        }
 
     }
     public ArrayList newOrder() {
