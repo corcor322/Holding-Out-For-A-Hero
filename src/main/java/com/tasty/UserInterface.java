210a83d7d6bca63f3dd58b6c7d71d5b2663428fa
corcor322 <corcor322@gmail.com>
2024-11-12 09:37:30 -0500

    Built UserInterface methods with temporary null return values

--- src/main/java/com/tasty/UserInterface.java
+++ src/main/java/com/tasty/UserInterface.java
@@ -1,4 +1,27 @@
 package com.tasty;
 
+import java.util.ArrayList;
+
 public class UserInterface {
+    public void display() {
+
+    }
+    public ArrayList newOrder() {
+        return null;
+    }
+    public Sandwich processAddSandwich() {
+        return null;
+    }
+    public Drink processAddDrink() {
+        return null;
+    }
+    public Chip processAddChip() {
+        return null;
+    }
+    public void processCheckOut() {
+
+    }
+    public void cancelOrder() {
+
+    }
 }
