3de83d92b6463874217d2ba65346ca81bd0a9f69
corcor322 <corcor322@gmail.com>
2024-11-13 09:15:41 -0500

    Refactored sandwich logic and moved add sandwich, drink, chip methods to Order class. Changed return types in interface to void.

--- src/main/java/com/tasty/Order.java
+++ src/main/java/com/tasty/Order.java
@@ -1,19 +1,98 @@
 package com.tasty;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Order {
-    private ArrayList sandwiches;
-    private ArrayList chips;
-    private ArrayList drinks;
+    private ArrayList<Sandwich> sandwiches;
+    private ArrayList<Chips> chips;
+    private ArrayList<Drink> drinks;
 
-    public void addSandwich() {
-
+    public Order() {
+        sandwiches = new ArrayList<>();
+        chips = new ArrayList<>();
+        drinks = new ArrayList<>();
     }
-    public void addChips() {
 
+    public void addSandwich(Scanner scanner) {
+        System.out.println("Select the size of sandwich you would like to add:");
+        System.out.println("Small- 4 inches/10.16 cm");
+        System.out.println("Medium- 8 inches/20.32 cm");
+        System.out.println("Large- 12 inches/30.48 cm");
+
+        String size = scanner.nextLine();
+
+        switch (size.toLowerCase()) {
+            case "small":
+            case "medium":
+            case "large":
+                break;
+            default:
+                System.out.println("Invalid choice. Please choose small, medium, or large.");
+                return; // Exit the method if the input is invalid
+        }
+
+        System.out.println("Select the type of bread you would like to add");
+        System.out.println("White");
+        System.out.println("Wheat");
+        System.out.println("Rye");
+        System.out.println("Wrap");
+
+        String bread = scanner.nextLine();
+
+        ArrayList<String> toppings = new ArrayList<>();
+        // Add logic for meat, cheese, regular toppings and sauce
+
+        System.out.println("Would you like your sandwich toasted? y/n");
+
+        String toasted = scanner.nextLine();
+        boolean isToasted = toasted.equalsIgnoreCase("y");
+
+        if (!toasted.equalsIgnoreCase("y") && !toasted.equalsIgnoreCase("n")) {
+            System.out.println("Invalid option. Please enter 'y' for yes or 'n' for 'no.'");
+            return; // Exit if the input is invalid
+        }
+
+        Sandwich sandwich = new Sandwich(size, bread, toppings, isToasted);
+        sandwiches.add(sandwich);
+        System.out.println("Sandwich added to the order!");
     }
-    public void addDrink() {
 
+    public void addDrink(Scanner scanner) {
+        System.out.println("Please select the type of drink you would like to add:");
+        System.out.println("Root Beer");
+        System.out.println("Orange Soda");
+        System.out.println("Grape Soda");
+        System.out.println("Cola");
+        System.out.println("Water");
+        System.out.println("Lemonade");
+        String type = scanner.nextLine();
+
+        System.out.println("Enter the size drink you would like:");
+        System.out.println("Small");
+        System.out.println("Medium");
+        System.out.println("Large");
+        String size = scanner.nextLine();
+
+        Drink drink = new Drink(type, size);
+        drinks.add(drink);
+        System.out.println("Drink added to the order!");
+    }
+
+    public void addChips(Scanner scanner) {
+        System.out.println("Please select the type of chip you would like to add:");
+        System.out.println("Salt & Vinegar");
+        System.out.println("Original");
+        System.out.println("Cheddar Sour Cream");
+        System.out.println("BBQ");
+        System.out.println("Hot Honey Jalapeño");
+        System.out.println("All-Dressed");
+
+        String type = scanner.nextLine();
+
+        Chips chip = new Chips(type);
+        chips.add(chip);
+        System.out.println("Chips added to the order!");
     }
 }
+
--- src/main/java/com/tasty/UserInterface.java
+++ src/main/java/com/tasty/UserInterface.java
@@ -1,5 +1,5 @@
 package com.tasty;
-//TODO: Add invalid exceptions for drink, transfer drink sandwich etc logic to Order class
+//TODO: Add invalid exceptions for drink
 import com.tasty.toppings.Meat;
 
 import java.util.ArrayList;
@@ -27,8 +27,8 @@ public class UserInterface {
         }
 
     }
-    public ArrayList newOrder(Scanner scanner) {
-        ArrayList order = new ArrayList<>();
+    public void newOrder(Scanner scanner) {
+        Order order = new Order();
         boolean quit = false;
 
         while (!quit) {
@@ -47,13 +47,13 @@ public class UserInterface {
 
             switch (choice) {
                 case "1":
-                    processAddSandwich(scanner);
+                    processAddSandwich(order, scanner);
                     System.out.println("Would you like to make that a combo with chips and a drink? Y/N");
                     String combo = scanner.nextLine();
 
                     if (combo.equalsIgnoreCase("y")) {
-                        processAddChip(scanner);
-                        processAddDrink(scanner);
+                        processAddChip(order, scanner);
+                        processAddDrink(order, scanner);
                         System.out.println("Combo successfully added.");
                     } else if (combo.equalsIgnoreCase("n")) {
                         System.out.println("Sandwich successfully added.");
@@ -61,111 +61,46 @@ public class UserInterface {
                         System.out.println("Invalid input. Please type 'y' for 'yes' or 'n' for 'no.'");
                     break;
                 case "2":
-                    processAddDrink(scanner);
+                    processAddDrink(order, scanner);
                     System.out.println("Drink added successfully.");
                     break;
                 case "3":
-                    processAddChip(scanner);
+                    processAddChip(order, scanner);
                     System.out.println("Chips added successfully.");
                     break;
                 case "4":
                     processCheckOut();
                     break;
                 case "0":
-                    cancelOrder();
+                    cancelOrder(scanner);
                     break;
                 default:
                     System.out.println("Invalid choice. Please type a number 0-4");
             }
         }
 
-        return order;
     }
-    public Sandwich processAddSandwich(Scanner scanner) {
-        System.out.println("Select the size of sandwich you would like to add:");
-        System.out.println("Small- 4 inches/10.16 cm");
-        System.out.println("Medium- 8 inches/20.32 cm");
-        System.out.println("Large- 12 inches/30.48 cm");
-
-        String size = scanner.nextLine();
-
-        switch (size) {
-            case "small":
-                break;
-            case "medium":
-                break;
-            case "large":
-                break;
-            default:
-                System.out.println("Invalid choice. Please choose small, medium, or large.");
-        }
-        System.out.println("Select the type of bread you would like to add");
-        System.out.println("White");
-        System.out.println("Wheat");
-        System.out.println("Rye");
-        System.out.println("Wrap");
-
-        String bread = scanner.nextLine();
-
-        // Add meats arraylist
-        // Add sauces arraylist
-        // Add cheeses arraylist
-        // Add regular toppings arraylist
-        // Add all toppings arraylist
-        ArrayList toppings = new ArrayList<>();
-
-        System.out.println("Would you like your sandwich toasted? y/n");
-
-        String toasted = scanner.nextLine();
-        boolean isToasted = false;
-
-        if(toasted.equalsIgnoreCase("y")) {
-            isToasted = true;
-        } else if (toasted.equalsIgnoreCase("n")) {
-            isToasted = false;
-        } else
-            System.out.println("Invalid option. Please enter 'y' for yes or 'n' for 'no.'");
-
-        return new Sandwich(size, bread, toppings, isToasted);
+    public void processAddSandwich(Order order, Scanner scanner) {
+        order.addSandwich(scanner);
     }
-    public Drink processAddDrink(Scanner scanner) {
-
-
-        System.out.println("Please select the type of drink you would like to add:");
-        System.out.println("Root Beer");
-        System.out.println("Orange Soda");
-        System.out.println("Grape Soda");
-        System.out.println("Cola");
-        System.out.println("Water");
-        System.out.println("Lemonade");
-        String type = scanner.nextLine();
-
-        System.out.println("Enter the size drink you would like:");
-        System.out.println("Small");
-        System.out.printf("Medium");
-        System.out.println("Large");
-        String size = scanner.nextLine();
-
-        return new Drink(type, size);
+    public void processAddDrink(Order order, Scanner scanner) {
+        order.addDrink(scanner);
     }
-    public Chips processAddChip(Scanner scanner) {
-
-        System.out.println("Please select the type of chip you would like to add:");
-        System.out.println("Salt & Vinegar");
-        System.out.println("Original");
-        System.out.println("Cheddar Sour Cream");
-        System.out.println("BBQ");
-        System.out.println("Hot Honey Jalapeño");
-        System.out.println("All-Dressed");
-
-        String type = scanner.nextLine();
-
-        return new Chips(type);
+    public void processAddChip(Order order, Scanner scanner) {
+        order.addChips(scanner);
     }
     public void processCheckOut() {
 
     }
-    public void cancelOrder() {
+    public void cancelOrder(Scanner scanner) {
+        System.out.println("Are you sure you want to cancel your order? Y/N");
+        String cancelOrder = scanner.nextLine();
 
+        if (cancelOrder.equalsIgnoreCase("y")) {
+
+        } else if (cancelOrder.equalsIgnoreCase("n")) {
+
+        } else
+            System.out.println("Invalid input. Please type 'y' to cancel order or 'n' to cancel.");
     }
 }
